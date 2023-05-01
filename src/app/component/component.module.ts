f94119fac80e725e1090268001bfc23877e21267
José Eduardo Ormeño Meneses <jormenom@odybank.com.pe>
2023-05-01 18:45:17 -0500

    fix: card en home

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [NavbarComponent,FooterComponent],
+  declarations: [NavbarComponent,FooterComponent,CardComponent],
   imports: [
     CommonModule
   ],exports:[
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    CardComponent
   ]
 })
 export class ComponentModule {
